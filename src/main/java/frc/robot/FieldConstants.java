9614b5d98cf366e5fe1bc8304be83adc09277fc9
Steph3n-T <135090615+Steph3n-T@users.noreply.github.com>
2025-03-07 20:33:35 -0700

    removed "tag relative" stuff because we want to do it differently

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -7,33 +7,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.subsystems.elbow.ElbowSubsystem;
 
 public final class FieldConstants {
-  record ReefPipePoses(Pose2d base, Pose2d L1, Pose2d L2, Pose2d L3, Pose2d L4) {
-    private static Pose2d offset(Pose2d base, ReefPipeLevel level) {
-      return new Pose2d(
-          base.getTranslation().plus(level.offset.getTranslation().rotateBy(base.getRotation())),
-          base.getRotation());
-      }
-
-    public ReefPipePoses(Pose2d base) {
-      this(
-          base,
-          offset(base, ReefPipeLevel.L1),
-          offset(base, ReefPipeLevel.L2),
-          offset(base, ReefPipeLevel.L3),
-          offset(base, ReefPipeLevel.L4));
-    }
-
-    public Pose2d getPose(ReefPipeLevel level) {
-      return switch (level) {
-        case BASE -> base;
-        case L1 -> L1;
-        case L2 -> L2;
-        case L3 -> L3;
-        case L4 -> L4;
-      };
-    }
-  }
-
   public Pose2d[] branchPoses = {
     new Pose2d(5, 5.247, Rotation2d.fromDegrees(-120)), //J
     new Pose2d(5.285, 5.107, Rotation2d.fromDegrees(-120)), //I
@@ -56,19 +29,6 @@ public final class FieldConstants {
     new Pose2d(16.40, 0.99, Rotation2d.fromDegrees(130)), // tag 1 CS
     new Pose2d(16.28, 7.58, Rotation2d.fromDegrees(-130)) // tag 2 CS
   };
-  public enum ReefPipeLevel {
-    BASE(Pose2d.kZero),
-    L1(new Pose2d(-0.60, 0, Rotation2d.kZero)),
-    L2(new Pose2d(-0.60, 0, Rotation2d.kZero)),
-    L3(new Pose2d(-0.60, 0, Rotation2d.kZero)),
-    L4(new Pose2d(-0.64635, 0, Rotation2d.kZero));
-  
-    public final Pose2d offset;
-  
-    private ReefPipeLevel(Pose2d offset) {
-      this.offset = offset;
-    }
-  }
 
   private static FieldConstants instance;
   
--- src/main/java/frc/robot/vision/LimelightLocalization.java
+++ src/main/java/frc/robot/vision/LimelightLocalization.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
 import frc.robot.FieldConstants;
-import frc.robot.FieldConstants.ReefPipeLevel;
 
 public class LimelightLocalization{
   public boolean rejectLeftData;
@@ -47,34 +46,6 @@ public class LimelightLocalization{
     LimelightHelpers.setPipelineIndex("limelight-middle", 0);
   }
 
-  record ReefPipePoses(Pose2d base, Pose2d L1, Pose2d L2, Pose2d L3, Pose2d L4) {
-    private static Pose2d offset(Pose2d base, ReefPipeLevel level) {
-      return new Pose2d(
-          base.getTranslation().plus(level.offset.getTranslation().rotateBy(base.getRotation())),
-          base.getRotation());
-      }
-
-    public ReefPipePoses(Pose2d base) {
-      this(
-          base,
-          offset(base, ReefPipeLevel.L1),
-          offset(base, ReefPipeLevel.L2),
-          offset(base, ReefPipeLevel.L3),
-          offset(base, ReefPipeLevel.L4));
-    }
-
-    public Pose2d getPose(ReefPipeLevel level) {
-      return switch (level) {
-        case BASE -> base;
-        case L1 -> L1;
-        case L2 -> L2;
-        case L3 -> L3;
-        case L4 -> L4;
-      };
-    }
-  }
-
-  
   public Pose2d[] getCoralStationPoses() {
     return Robot.alliance.get() == Alliance.Red ? coralStationPosesRed : coralStationPosesBlue;
   }
