7ec969c0872ca3f8c4ff89de736ac091261f290e
GavinDistel <69681028+Gryffindor528@users.noreply.github.com>
2025-04-05 09:25:12 -0700

    changed angle lock boolean name

--- src/main/java/frc/robot/commands/RobotManager.java
+++ src/main/java/frc/robot/commands/RobotManager.java
@@ -927,19 +927,19 @@ public class RobotManager extends StateMachine<RobotState> {
   }
 
   public void autoReefAlignRequest(){
-    if(DrivetrainSubsystem.getInstance().rizzlerMode){
+    if(DrivetrainSubsystem.getInstance().crescendoModeEnabled){
     DrivetrainSubsystem.getInstance().setState(DrivetrainState.AUTO_REEF_ALIGN_1);
     }
   }
 
   public void autoAlgaeAlignRequest(){
-    if(DrivetrainSubsystem.getInstance().rizzlerMode){
+    if(DrivetrainSubsystem.getInstance().crescendoModeEnabled){
     DrivetrainSubsystem.getInstance().setState(DrivetrainState.AUTO_ALGAE_ALIGN);
     }
   }
 
   public void autoCoralStationAlignRequest(){
-    if(DrivetrainSubsystem.getInstance().rizzlerMode){
+    if(DrivetrainSubsystem.getInstance().crescendoModeEnabled){
     DrivetrainSubsystem.getInstance().setState(DrivetrainState.AUTO_CORAL_STATION_ALIGN_1);
     }
   }
