f56e4bc0cf0723849bb90a5d7db544db6d24761f
gatlinfarrington <gatlinf@utexas.edu>
2025-09-24 19:19:52 -0700

    The robot is in shambles

--- src/main/java/frc/robot/subsystems/wrist/WristPositions.java
+++ src/main/java/frc/robot/subsystems/wrist/WristPositions.java
@@ -1,27 +1,28 @@
 package frc.robot.subsystems.wrist;
 
 public class WristPositions {
-    public static double IDLE = 0.46;
-    public static final double PRE_L4 = 0.1;
-    public static double L1 = 0.1; // -0.02
-    public static double L1_ROW1 = 0.1; // -0.02
-    public static double L1_ROW2 = 0.1; // -0.02
-    public static final double L2 = 0.45;
-    public static final double L3 = 0.45;
-    public static final double L4_WRIST = 0.475; //0.475
-    public static final double CAPPED_L4 = 0.1;
-    public static final double L4_TRANSITION = 0.1;
-    public static final double CORAL_STATION = 0.125;
-    public static double INVERTED_IDLE = 0.339; //0.224
-    public static final double INVERTED_CORAL_STATION = 0.09; //0.055
-    public static final double PRE_ALGAE_SCORE = 0.25;
-    public static final double FRONT_ALGAE_SCORE = 0.04;
-    public static final double ALGAE_FLICK = 0.085;
-    public static final double ALGAE_SCORE = 0.04;
-    public static final double ALGAE_INTAKE = 0.32;
-    public static final double GROUND_ALGAE_INTAKE = 0.475; //0.44
-    public static final double FAILSAFE_GROUND_ALGAE_INTAKE = 0.38;
-    public static final double PROCESSOR = 0.492;
-    public static final double CAGE_FLIP = 0.55;
-    public static final double FUNNEL_INTAKE = 0.235;
+    public static double POS_OFFSET = .1;
+    public static double IDLE = 0.46 - POS_OFFSET;
+    public static final double PRE_L4 = 0.1 - POS_OFFSET;
+    public static double L1 = 0.1 - POS_OFFSET; // -0.02
+    public static double L1_ROW1 = 0.1 - POS_OFFSET; // -0.02
+    public static double L1_ROW2 = 0.1 - POS_OFFSET; // -0.02
+    public static final double L2 = 0.45 - POS_OFFSET;
+    public static final double L3 = 0.45 - POS_OFFSET;
+    public static final double L4_WRIST = 0.475 - POS_OFFSET; //0.475
+    public static final double CAPPED_L4 = 0.1 - POS_OFFSET;
+    public static final double L4_TRANSITION = 0.1 - POS_OFFSET;
+    public static final double CORAL_STATION = 0.125 - POS_OFFSET;
+    public static double INVERTED_IDLE = 0.339 - POS_OFFSET; //0.224
+    public static final double INVERTED_CORAL_STATION = 0.09 - POS_OFFSET; //0.055
+    public static final double PRE_ALGAE_SCORE = 0.25 - POS_OFFSET;
+    public static final double FRONT_ALGAE_SCORE = 0.04 - POS_OFFSET;
+    public static final double ALGAE_FLICK = 0.085 - POS_OFFSET;
+    public static final double ALGAE_SCORE = 0.04 - POS_OFFSET;
+    public static final double ALGAE_INTAKE = 0.32 - POS_OFFSET;
+    public static final double GROUND_ALGAE_INTAKE = 0.475 - POS_OFFSET; //0.44
+    public static final double FAILSAFE_GROUND_ALGAE_INTAKE = 0.38 - POS_OFFSET;
+    public static final double PROCESSOR = 0.492 - POS_OFFSET;
+    public static final double CAGE_FLIP = 0.55 - POS_OFFSET;
+    public static final double FUNNEL_INTAKE = 0.235 - POS_OFFSET;
 }
